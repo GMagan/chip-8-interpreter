746db4aa11bdc3b3a3fa134276fcead899de3344
GMagan <guilherme_magan@hotmail.com>
2026-03-10 15:57:23 -0300

    feat: added more variables and implemented the display

--- Code/c8vm.h
+++ Code/c8vm.h
@@ -1,18 +1,27 @@
 #include <stdint.h>
+#include "display.h"
 
-typedef struct VM {
-   uint8_t RAM[4096];         // Memória 4KB
-   uint16_t PC;               // Program Counter
-   uint8_t V[16];             // Registradores de propósito geral
-   uint8_t SP;                // Stack Pointer
-   uint16_t I;                // Registrador de índice
-   uint16_t stack[16];        // Pilha
-   uint8_t DISPLAY[64 * 32];  // Tela
-   uint8_t delay_timer;
-   uint8_t sound_timer;
-} VM;
+class VM
+{
+private:
+   uint8_t RAM[4096];   // Memória 4KB
+   uint16_t PC;         // Contador de programa
+   uint8_t V[16];       // Registradores V0–VF
+   uint8_t SP;          // Ponteiro da pilha
+   uint16_t I;          // Registrador de índice
+   uint16_t stack[16];  // Pilha de 16 níveis
+   uint8_t delay_timer; // Timer de delay
+   uint8_t sound_timer; // Timer de som
+   uint8_t teclado[16]; // Estado do teclado (16 teclas)
 
-void VM_Inicializar(VM *vm, uint16_t pc_inicial);
-void VM_CarregarROM(VM *vm, char *arq_rom, uint16_t pc_inicial);
-void VM_ExecutarInstrucao(VM *vm);
-void VM_ImprimirRegistradores(VM *vm);
+public:
+   Display DISPLAY; // Display do sistema
+
+   VM(int pixel_scale = 10); // Construtor
+
+   void carregarROM(const char *arq_rom, uint16_t pc_inicial); // Carrega ROM na memória
+   void executarInstrucao();                                   // Executa uma instrução
+   void imprimirRegistradores();                               // Imprime registradores no console
+   void definirTecla(uint8_t tecla, bool apertada);            // Define estado da tecla
+   void atualizarTimers();                                     // Atualiza timers (delay e som)
+};
